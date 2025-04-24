1a20dabd52a6e1e627c25798ed14539a05d9366e
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2025-04-23 23:00:45 -0500

    added infinite scroll feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBar from "./components/SearchBar";
 import searchImages from "./api";
 import ImageList from "./components/ImageList";
 
 function App() {
   const [images, setImages] = useState([]);
-  const handleSubmit = async (term) => {
-    const result = await searchImages(term);
-    setImages(result);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [term, setTerm] = useState("");
+  const [loading, setLoading] = useState(false); // Prevent multiple calls
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isBottom =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight - 10;
+
+      if (isBottom && !loading) {
+        setPageNumber((prev) => prev + 1);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [loading]);
+
+  useEffect(() => {
+    const fetchMoreImages = async () => {
+      if (term) {
+        setLoading(true);
+        const result = await searchImages(term, pageNumber);
+        setImages((prev) => (pageNumber === 1 ? result : [...prev, ...result]));
+        setLoading(false);
+      }
+    };
+    fetchMoreImages();
+  }, [pageNumber, term]);
+
+  const handleSubmit = async (newTerm) => {
+    setTerm(newTerm);
+    setPageNumber(1);
   };
+
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const searchImages = async (term) => {
+const searchImages = async (term, page) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID 8O50V7bNzfKdVixwS9W9nZVdr0VnrCv9gmeimfdvp6Y",
     },
     params: {
       query: term,
+      page: page,
     },
   });
 
